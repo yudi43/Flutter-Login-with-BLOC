0beb1bc627093066291873861de9d8504f882195
Yudhishthir Singh <singh.yudi10@gmail.com>
2020-02-16 19:57:11 +0530

    Login UI

--- flutter_logic_bloc/lib/src/screens/login_screen.dart
+++ flutter_logic_bloc/lib/src/screens/login_screen.dart
@@ -6,8 +6,42 @@ class LoginScreen extends StatelessWidget {
     return Container(
       margin: EdgeInsets.all(20.0),
       child: Column(
-        children: <Widget>[],
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          emailField(),
+          passwordField(),
+          Container(margin: EdgeInsets.only(top: 25)),
+          submitButton(),
+        ],
       ),
+    );
+  }
+
+  Widget emailField() {
+    return TextField(
+      keyboardType: TextInputType.emailAddress,
+      decoration: InputDecoration(
+        hintText: "you@example.com",
+        labelText: "Email Address",
+      ),
+    );
+  }
+
+  Widget passwordField() {
+    return TextField(
+      obscureText: true,
+      decoration: InputDecoration(
+        hintText: "Password",
+        labelText: "Password",
+      ),
+    );
+  }
+
+  Widget submitButton() {
+    return RaisedButton(
+      child: Text("Login"),
+      color: Colors.blue,
+      onPressed: () {},
     );
   }
 }
